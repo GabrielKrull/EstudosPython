a3573a5ed74333e86c78e0b2f2905a8999035159
Gabriel Alves Krull <bielpoow@gmail.com>
2025-11-06 16:22:22 -0300

    estudos 06-11

---
+++ aula007ex014.py
@@ -0,0 +1,4 @@
+celsius = float(input("Digite uma temperatura de Celsius: "))
+fharenheit = (celsius * 1.8) + 32
+
+print(f"A temperatura {celsius} graus celsius em fharenheit é de {fharenheit}")
---
+++ aula007ex015.py
@@ -0,0 +1,6 @@
+km = int(input("Quantos km foram rodados: "))
+dia = int(input("Quantos dias de uso: "))
+
+conta = dia * 60 + km * 0.15
+
+print(f"O total que deve-se pagar é de: {conta}")
